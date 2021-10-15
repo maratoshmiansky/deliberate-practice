c274e64b5028f3f5386d05a2654cf0e9a1852a6b
Marat Oshmiansky <maratical@gmail.com>
2021-10-15 12:17:08 -0700

    add more nested loops problems

--- deliberate_practice1.js
+++ deliberate_practice1.js
@@ -6,23 +6,6 @@
 // Output: [12, 15, 18, 20, 24, 30]
 // Explanation: (3 * 4, 3 * 5, 3 * 6, 4 * 5, 4 * 6, 5 * 6
 
-// def pair_products(array)
-//   pairs = []
-//   index1 = 0
-
-//   while index1 < array.length
-//     index2 = index1 + 1
-//     while index2 < array.length
-//       pairs << array[index1] * array[index2]
-//       index2 += 1
-//     end
-//     index1 += 1
-//   end
-//   pairs
-// end
-
-// p pair_products([3, 4, 5, 6])
-
 function pairProducts(array) {
   let pairs = [];
   let index1 = 0;
@@ -46,23 +29,6 @@ console.log(pairProducts([3, 4, 5, 6]));
 // Input: Two arrays: [1, 2], [3, 4, 5]
 // Output: [3, 4, 5, 6, 8, 10]
 // Explanation: (1 * 3, 1 * 4, 1 * 5, 2 * 3, 2 * 4, 2 * 5)
-
-// def products_both(array1, array2)
-//   products = []
-//   index1 = 0
-
-//   while index1 < array1.length
-//     index2 = 0
-//     while index2 < array2.length
-//       products << array1[index1] * array2[index2]
-//       index2 += 1
-//     end
-//     index1 += 1
-//   end
-//   products
-// end
-
-// p products_both([1, 2], [3, 4, 5])
 
 function productsBoth(array1, array2) {
   let products = [];
@@ -92,23 +58,23 @@ console.log(productsBoth([1, 2], [3, 4, 5]));
 // Input: [1, 2, 3, 4]
 // Output: false (because there is no combination of two numbers that add up to 10)
 
-// def pair_sums_to_10(array)
-//   sums = []
-//   index1 = 0
+function pairSumsTo10(array) {
+  let sums = [];
+  let index1 = 0;
 
-//   while index1 < array.length
-//     index2 = index1 + 1
-//     while index2 < array.length
-//       sums << array[index1] + array[index2]
-//       index2 += 1
-//     end
-//     index1 += 1
-//   end
-//   sums.include?(10)
-// end
+  while (index1 < array.length) {
+    let index2 = index1 + 1;
+    while (index2 < array.length) {
+      sums.push(array[index1] + array[index2]);
+      index2++;
+    }
+    index1++;
+  }
+  return sums.includes(10);
+}
 
-// p pair_sums_to_10([2, 4, 6, 7])
-// p pair_sums_to_10([1, 2, 3, 4])
+console.log(pairSumsTo10([2, 4, 6, 7]));
+console.log(pairSumsTo10([1, 2, 3, 4]));
 
 //4
 // Write a function that accepts an array of numbers, and returns the largest product of any pair of numbers within a given array.
@@ -118,23 +84,23 @@ console.log(productsBoth([1, 2], [3, 4, 5]));
 // Output: 63
 // Explanation: (This is -9 * -7)
 
-// def largest_pair_product(array)
-//   products = []
-//   index1 = 0
+function largestPairProduct(array) {
+  let products = [];
+  let index1 = 0;
 
-//   while index1 < array.length
-//     index2 = index1 + 1
-//     while index2 < array.length
-//       products << array[index1] * array[index2]
-//       index2 += 1
-//     end
-//     index1 += 1
-//   end
-//   products.max
-// end
+  while (index1 < array.length) {
+    let index2 = index1 + 1;
+    while (index2 < array.length) {
+      products.push(array[index1] * array[index2]);
+      index2++;
+    }
+    index1++;
+  }
+  return Math.max(...products);
+}
 
-// p largest_pair_product([5, -2, 1, -9, -7, 2, 6])
-// p largest_pair_product([-100, -10, -1, 0, 1, 10, 100])
+console.log(largestPairProduct([5, -2, 1, -9, -7, 2, 6]));
+console.log(largestPairProduct([-100, -10, -1, 0, 1, 10, 100]));
 
 //5
 // Write a function that accepts two arrays of numbers, and returns the largest sum of a number from the first array added to a number from the second array.
