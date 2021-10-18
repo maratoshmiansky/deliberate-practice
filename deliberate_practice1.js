88e6b563455f5940872625246185bda448bae6ff
Marat Oshmiansky <maratical@gmail.com>
2021-10-18 10:39:23 -0700

    add functions of two arrays

--- deliberate_practice1.js
+++ deliberate_practice1.js
@@ -21,7 +21,7 @@ function pairProducts(array) {
   return pairs;
 }
 
-console.log(pairProducts([3, 4, 5, 6]));
+console.log(pairProducts([3, 4, 5, 6])); // => [12, 15, 18, 20, 24, 30]
 
 //2
 // Write a function that accepts two arrays of numbers, and returns an array of the products of every number from the first array multiplied by every number of the second array:
@@ -45,7 +45,7 @@ function productsBoth(array1, array2) {
   return products;
 }
 
-console.log(productsBoth([1, 2], [3, 4, 5]));
+console.log(productsBoth([1, 2], [3, 4, 5])); // => [3, 4, 5, 6, 8, 10]
 
 //3
 // Write a function that accepts an array of numbers and returns true or false, depending on whether there is a pair of two numbers that add up to 10.
@@ -73,8 +73,8 @@ function pairSumsTo10(array) {
   return sums.includes(10);
 }
 
-console.log(pairSumsTo10([2, 4, 6, 7]));
-console.log(pairSumsTo10([1, 2, 3, 4]));
+console.log(pairSumsTo10([2, 4, 6, 7])); // => true
+console.log(pairSumsTo10([1, 2, 3, 4])); // => false
 
 //4
 // Write a function that accepts an array of numbers, and returns the largest product of any pair of numbers within a given array.
@@ -99,8 +99,8 @@ function largestPairProduct(array) {
   return Math.max(...products);
 }
 
-console.log(largestPairProduct([5, -2, 1, -9, -7, 2, 6]));
-console.log(largestPairProduct([-100, -10, -1, 0, 1, 10, 100]));
+console.log(largestPairProduct([5, -2, 1, -9, -7, 2, 6])); // -9 * -7 = 63
+console.log(largestPairProduct([-100, -10, -1, 0, 1, 10, 100])); // -100 * -10 = 10 * 100 = 1000
 
 //5
 // Write a function that accepts two arrays of numbers, and returns the largest sum of a number from the first array added to a number from the second array.
@@ -110,12 +110,12 @@ console.log(largestPairProduct([-100, -10, -1, 0, 1, 10, 100]));
 // Output: 13
 // Explanation: (5 from the first array + 8 from the second array)
 
-// def largest_sum(array1, array2)
-//   array1.max + array2.max
-// end
+function largestSum(array1, array2) {
+  return Math.max(...array1) + Math.max(...array2);
+}
 
-// p largest_sum([5, 2, 1], [7, 8, 4, 1])
-// p largest_sum([1, 3, 5, 7, 9], [14, 9, -3, 0, 1, 12])  # => 9 + 14 = 23
+console.log(largestSum([5, 2, 1], [7, 8, 4, 1])); // 5 + 8 = 13
+console.log(largestSum([1, 3, 5, 7, 9], [14, 9, -3, 0, 1, 12])); // 9 + 14 = 23
 
 //6
 // Write a function that accepts two arrays of numbers, and returns an array that is the “intersection” of the two arrays, meaning an array of the numbers that are shared by the two original arrays.
@@ -124,18 +124,18 @@ console.log(largestPairProduct([-100, -10, -1, 0, 1, 10, 100]));
 // Output: [2, 4] (or [4, 2])
 // Explanation: 2 and 4 are the only values shared by both input arrays.
 
-// def intersection(array1, array2)
-//   both = []
-//   index = 0
-//   while index < array1.length
-//     if array2.include?(array1[index]) && !both.include?(array1[index])
-//       both << array1[index]
-//     end
-//     index += 1
-//   end
-//   both
-// end
+function intersection(array1, array2) {
+  let both = [];
+  let index = 0;
+  while (index < array1.length) {
+    if (array2.includes(array1[index]) && !both.includes(array1[index])) {
+      both.push(array1[index]);
+    }
+    index++;
+  }
+  return both;
+}
 
-// p intersection([5, 4, 3, 2, 1], [0, 2, 4, 6])  # => [4, 2]
-// p intersection([1, 2, 1, 2, 1, 2], [1, 2, 3, 4])  # => [1, 2]
-// p intersection([17, 31, 9], [8, 24, 13])  # => []
+console.log(intersection([5, 4, 3, 2, 1], [0, 2, 4, 6])); // => [4, 2]
+console.log(intersection([1, 2, 1, 2, 1, 2], [1, 2, 3, 4])); // => [1, 2]
+console.log(intersection([17, 31, 9], [8, 24, 13])); // => []
