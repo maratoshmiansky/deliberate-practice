7030129371732e650995a7fe1b4f6bfe7f511f49
Marat Oshmiansky <maratical@gmail.com>
2021-11-08 13:22:36 -0800

    add object functions with for..of

--- objects3.js
+++ objects3.js
@@ -8,9 +8,9 @@
 function objectToArray(inputObject) {
   let outputArray = [];
   let index = 0;
-  for (const objectKey in inputObject) {
+  for (const [objectKey, objectValue] of Object.entries(inputObject)) {
     outputArray[index] = objectKey;
-    outputArray[index + 1] = inputObject[objectKey];
+    outputArray[index + 1] = objectValue;
     index += 2;
   }
   return outputArray;
@@ -23,44 +23,53 @@ console.log(objectToArray({ a: 1, b: 2, c: 3, d: 4 })); // => ['a', 1, 'b', 2, '
 // Input: {"chair": 100, "book": 14, "sofa": 55}
 // Output: [ ["chair", 100], ["book", 14], ["sofa", 55] ]
 
-// def hash_to_array2(hash)
-//   array = []
-//   hash.each { |key, value| array << [key, value] }
-//   array
-// end
+function objectToArray2(inputObject) {
+  return Object.entries(inputObject);
+}
 
-// p hash_to_array2({"a": 1, "b": 2, "c": 3, "d": 4})  # this returns the string keys as symbols in the subarrays
-// p hash_to_array2({"a" => 1, "b" => 2, "c" => 3, "d" => 4})  # this returns the string keys as strings in the subarrays
+console.log(objectToArray2({ chair: 100, book: 14, sofa: 55 })); // => [ [ 'chair', 100 ], [ 'book', 14 ], [ 'sofa', 55 ] ]
+console.log(objectToArray2({ a: 1, b: 2, c: 3, d: 4 })); // => [ [ 'a', 1 ], [ 'b', 2 ], [ 'c', 3 ], [ 'd', 4 ] ]
 
 //3
 // Let's do the inverse of the previous exercise. Write a function that accepts an array of subarrays, and return a hash table - in this format:
 // Input: [ [1, 3], [8, 9], [2, 16] ]
 // Output: becomes {1: 3, 8: 9, 2: 16}
 
-// def array_to_hash(array)
-//   hash = {}
-//   array.each { |subarray| hash[subarray[0]] = subarray[1] }
-//   hash
-// end
+function arrayToObject(inputArray) {
+  return Object.fromEntries(inputArray);
+}
 
-// p array_to_hash([[1, 3], [8, 9], [2, 16]])  # => {1 => 3, 8 => 9, 2 => 16}
-// p array_to_hash([["I", 1], ["me", 2], ["mine", 3]])  # => {"I" => 1, "me" => 2, "mine" => 3}
+console.log(
+  arrayToObject([
+    [1, 3],
+    [8, 9],
+    [2, 16],
+  ])
+); // => { '1': 3, '2': 16, '8': 9 }
+console.log(
+  arrayToObject([
+    ["I", 1],
+    ["me", 2],
+    ["mine", 3],
+  ])
+); // => { I: 1, me: 2, mine: 3 }
 
 //4
 // Write a function that accepts a hash table and returns a new hash table with the keys and values flipped:
 // Input: {"a": 1, "b": 2, "c": 3}
 // Output: {1: "a", 2: "b", 3: "c"}
 
-// def flip_hash(hash)
-//   flipped_hash = {}
-//   hash.each { |key, value| flipped_hash[value] = key }
-//   flipped_hash
-// end
+function flipObject(inputObject) {
+  let flippedObject = {};
+  for (const [objectKey, objectValue] of Object.entries(inputObject)) {
+    flippedObject[objectValue] = objectKey;
+  }
+  return flippedObject;
+}
 
-// p flip_hash({"a": 1, "b": 2, "c": 3})  # string keys are converted to symbol values
-// p flip_hash({"a" => 1, "b" => 2, "c" => 3})  # string keys become string values
-// p flip_hash({"Joe" => 1, "Jim" => 2, "Jack" => 1})  # => {1 => "Jack", 2 => "Jim"}
-// p flip_hash({1 => "Joe", 2 => "Jim", 1 => "Jack"})  # => {"Jack" => 1, "Jim" => 2}
+console.log(flipObject({ a: 1, b: 2, c: 3 })); // => { '1': 'a', '2': 'b', '3': 'c' }
+console.log(flipObject({ Joe: 1, Jim: 2, Jack: 1 })); // => { '1': 'Jack', '2': 'Jim' }
+console.log(flipObject({ 1: "Joe", 2: "Jim", 3: "Jack" })); // => { Joe: '1', Jim: '2', Jack: '3' }
 
 // 5
 // Write a function that accepts an array of strings and returns a hash table containing the number of times each string occurs:
