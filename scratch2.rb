079664550456925b2ecfd4497533e2a47a5920ae
Marat Oshmiansky <maratical@gmail.com>
2021-12-05 22:17:46 -0800

    add classy find_match method

--- scratch2.rb
+++ scratch2.rb
@@ -23,6 +23,8 @@ p CalculateAge.calculate(Date.new(1987, 2, 1), Date.new(1987, 1, 1))  # => "deat
 
 #2 Match people (in array of hashes) by first name, middle name and/or last name keyword arguments
 
+# First implementation (classless)
+
 def find_match(first_name: "", middle_name: "", last_name: "")  # set each keyword parameter value to "" by default
   people = [
     { first_name: "Michael", middle_name: "Peter", last_name: "Johnson" },
@@ -31,6 +33,7 @@ def find_match(first_name: "", middle_name: "", last_name: "")  # set each keywo
     { first_name: "Hannah", middle_name: "Barbara", last_name: "Olsen" },
   ]
 
+  return [] if first_name.empty? && middle_name.empty? && last_name.empty?  # return empty array if no name data passed
   people.select { |person| (person[:first_name] == first_name || first_name.empty?) && (person[:middle_name] == middle_name || middle_name.empty?) && (person[:last_name] == last_name || last_name.empty?) }  # select all people with matching name arguments only (filtering out missing name argument(s))
 end
 
@@ -45,3 +48,52 @@ p find_match(last_name: "Livingstone")  # => [{:first_name=>"Jane", :middle_name
 p find_match(last_name: "Livingstone", first_name: "Jane")  # => [{:first_name=>"Jane", :middle_name=>"Barbara", :last_name=>"Livingstone"}]
 p find_match(middle_name: "Lawrence")  # => [{:first_name=>"Michael", :middle_name=>"Lawrence", :last_name=>"Johnson"}]
 p find_match  # => []
+
+# Second implementation (classy)
+
+class PersonMatcher
+  attr_reader :people
+
+  def initialize(*people)
+    @people = people
+  end
+
+  def find_match(first_name: "", middle_name: "", last_name: "")  # set each keyword parameter value to "" by default
+    return [] if first_name.empty? && middle_name.empty? && last_name.empty?  # return empty array if no name data passed
+    people.select { |person| (person[:first_name] == first_name || first_name.empty?) && (person[:middle_name] == middle_name || middle_name.empty?) && (person[:last_name] == last_name || last_name.empty?) }  # select all people with matching name arguments only (filtering out missing name argument(s))
+  end
+end
+
+dwight1 = { first_name: "Dwight", middle_name: "Glenn", last_name: "Appleton" }
+dwight2 = { first_name: "Dwight", middle_name: "Gerald", last_name: "Appleton" }
+autumn = { first_name: "Autumn", middle_name: "Harley", last_name: "Poole" }
+noah = { first_name: "Noah", middle_name: "Zaire", last_name: "Waters" }
+
+people = [dwight1, dwight2, autumn, noah]
+
+instance = PersonMatcher.new(*people)
+
+p instance.find_match(first_name: "Dwight", last_name: "Appleton")  # => [{:first_name=>"Dwight", :middle_name=>"Glenn", :last_name=>"Appleton"}, {:first_name=>"Dwight", :middle_name=>"Gerald", :last_name=>"Appleton"}]
+p instance.find_match(first_name: "Dwight", middle_name: "Gerald", last_name: "Appleton")  # => [{:first_name=>"Dwight", :middle_name=>"Gerald", :last_name=>"Appleton"}]
+p instance.find_match  # => []
+
+peeps = [
+  { first_name: "Michael", middle_name: "Peter", last_name: "Johnson" },
+  { first_name: "Michael", middle_name: "Lawrence", last_name: "Johnson" },
+  { first_name: "Jane", middle_name: "Barbara", last_name: "Livingstone" },
+  { first_name: "Hannah", middle_name: "Barbara", last_name: "Olsen" },
+]
+
+mypeeps = PersonMatcher.new(*peeps)
+
+p mypeeps.find_match(first_name: "Michael", last_name: "Johnson")  # => [{:first_name=>"Michael", :middle_name=>"Peter", :last_name=>"Johnson"}, {:first_name=>"Michael", :middle_name=>"Lawrence", :last_name=>"Johnson"}]
+p mypeeps.find_match(middle_name: "Barbara")  # => [{:first_name=>"Jane", :middle_name=>"Barbara", :last_name=>"Livingstone"}, {:first_name=>"Hannah", :middle_name=>"Barbara", :last_name=>"Olsen"}]
+p mypeeps.find_match(first_name: "Barbara")  # => []
+p mypeeps.find_match(first_name: "Jane")  # => [{:first_name=>"Jane", :middle_name=>"Barbara", :last_name=>"Livingstone"}]
+p mypeeps.find_match(first_name: "Michael", last_name: "Harris")  # => []
+p mypeeps.find_match(first_name: "Michael", middle_name: "Peter", last_name: "Johnson")  # => [{:first_name=>"Michael", :middle_name=>"Peter", :last_name=>"Johnson"}]
+p mypeeps.find_match(first_name: "Hannah", middle_name: "Barbara")  # => [{:first_name=>"Hannah", :middle_name=>"Barbara", :last_name=>"Olsen"}]
+p mypeeps.find_match(last_name: "Livingstone")  # => [{:first_name=>"Jane", :middle_name=>"Barbara", :last_name=>"Livingstone"}]
+p mypeeps.find_match(last_name: "Livingstone", first_name: "Jane")  # => [{:first_name=>"Jane", :middle_name=>"Barbara", :last_name=>"Livingstone"}]
+p mypeeps.find_match(middle_name: "Lawrence")  # => [{:first_name=>"Michael", :middle_name=>"Lawrence", :last_name=>"Johnson"}]
+p mypeeps.find_match  # => []
